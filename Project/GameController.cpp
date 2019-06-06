3bcf9b98c34d988925730d219514a22d4e6763ae
LielCohen1997 <liel1997@gmail.com>
2019-06-06 18:15:59 +0300

    last update

--- Project/GameController.cpp
+++ Project/GameController.cpp
@@ -93,7 +93,7 @@ void GameController::run()
 		m_window.clear(sf::Color::White);
 		m_window.draw(m_back);
 		print();
-		// m_world->DrawDebugData();
+		m_world->DrawDebugData();
 		m_window.display();
 		eventhandler();
 
